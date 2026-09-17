8d75474854add414b01263772332b81d95ec6998
Ahsan_rizvi <ahsanrizvi31@gmail.com>
2026-09-17 19:36:41 +0500

    Update proc_bronze.sql

--- scripts/bronze/proc_bronze.sql
+++ scripts/bronze/proc_bronze.sql
@@ -6,3 +6,12 @@ WITH (
     DELIMITER ',',
     QUOTE '"'
 );
+
+COPY bronze.orders
+FROM 'C:/Users/ahsan/Downloads/Temp/olist_orders_dataset.csv'
+WITH (
+	FORMAT CSV,
+	HEADER TRUE,
+	DELIMITER ',',
+	QUOTE '"'
+);
