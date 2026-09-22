5cc50e51de24f8dee9d5bfb366d73137338a8ef9
Ahsan_rizvi <ahsanrizvi31@gmail.com>
2026-09-22 17:32:45 +0500

    Update customers_dq.sql

--- data_quality_checks/customers_dq.sql
+++ data_quality_checks/customers_dq.sql
@@ -2,6 +2,7 @@
 
 
 -- check the columns and idenfity the data types and nulls
+  
 SELECT 
 column_name,
 data_type,
@@ -11,6 +12,7 @@ WHERE table_name = 'customers'
 AND table_schema = 'bronze'
 
 -- identify the total_count
+  
 SELECT 
 COUNT(*) AS row_count
 FROM bronze.customers
@@ -26,6 +28,7 @@ GROUP BY customer_id
 HAVING count(*) > 1 OR NULL
 
 -- duplicate and null check in customer_unique_id
+  
 SELECT 
 customer_unique_id,
 COUNT(*) AS total_counts
@@ -34,6 +37,7 @@ GROUP BY customer_unique_id
 HAVING COUNT(*) > 1 OR NULL
 
 -- lenght and null check in customer_zip_code_prefix
+  
 SELECT 
 customer_zip_code_prefix,
 LENGTH(customer_zip_code_prefix) AS len
@@ -42,12 +46,14 @@ GROUP BY customer_zip_code_prefix
 HAVING LENGTH(customer_zip_code_prefix) < 5 OR NULL
 
 --null and empty string check in customer city
+  
 SELECT 
 customer_city
 FROM bronze.customers
 WHERE customer_city = '' OR customer_city IS NULL 
 
 -- checking unwanted spaces in customer_city
+  
 SELECT 
 customer_city
 FROM bronze.customers
@@ -55,6 +61,7 @@ WHERE customer_city != TRIM(customer_city)
 
 
 -- checking data standardizaton and consistency
+  
 SELECT DISTINCT
 customer_state
 FROM bronze.customers
