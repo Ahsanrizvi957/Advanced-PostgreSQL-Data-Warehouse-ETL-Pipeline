86f4bd0087e5ed2c60617b0ffcf0b6e03c9aaeb3
Ahsan_rizvi <ahsanrizvi31@gmail.com>
2026-09-24 19:20:53 +0500

    Create ddl_silver.sql

---
+++ scripts/silver/ddl_silver.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS silver.customers;
+CREATE TABLE silver.customers(
+
+	customer_id              VARCHAR(50) NOT NULL,
+	customer_unique_id       VARCHAR(50) NOT NULL,
+	customer_zip_code_prefix VARCHAR(5)  NOT NULL,
+	customer_city 			 VARCHAR(50) NOT NULL,
+	customer_state 			 CHAR(2)     NOT NULL
+);
