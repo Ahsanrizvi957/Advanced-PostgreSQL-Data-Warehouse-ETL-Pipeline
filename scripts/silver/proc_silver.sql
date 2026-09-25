5e7151615ec1eb5a702cc746743398a3a3fd6f84
Ahsan_rizvi <ahsanrizvi31@gmail.com>
2026-09-25 15:16:28 +0500

    Update proc_silver.sql

--- scripts/silver/proc_silver.sql
+++ scripts/silver/proc_silver.sql
@@ -7,9 +7,33 @@ INSERT INTO silver.customers(
 	customer_state 			 
 )
 SELECT 
-customer_id::VARCHAR (50),
+customer_id::VARCHAR (50), -- converting text to varchar
 customer_unique_id::VARCHAR (50),
 customer_zip_code_prefix::VARCHAR (5),
 customer_city::VARCHAR (50),
 customer_state::VARCHAR (2)
 FROM bronze.customers
+
+TRUNCATE TABLE silver.orders;
+INSERT INTO silver.orders(
+
+order_id,
+customer_id,
+order_status,
+order_purchase_timestamp,
+order_approved_at,
+order_delivered_carrier_date,
+order_delivered_customer_date,
+order_estimated_delivery_date
+
+)
+SELECT
+order_id,
+customer_id,
+order_status,
+order_purchase_timestamp::TIMESTAMP, -- converting text to timestamp
+order_approved_at::TIMESTAMP,
+order_delivered_carrier_date::TIMESTAMP,
+order_delivered_customer_date::TIMESTAMP,
+order_estimated_delivery_date::DATE --converting text to date
+FROM bronze.orders;
